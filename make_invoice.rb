e541b8cf23945129231d42eaf44ebc6814bec4f0
Greg McGuirk <bert.mcguirk@gmail.com>
2015-08-16 15:44:07 -0700

    Create CLI for creating invoices
    For now, a simple loop will work for these data.  Eventually, if
    it gets more complicated we can move to THOR.
    
    The make_invoice file now exectues the run function that will
    let the user enter any number of line items to create one invoice. This
    system helps take the data out of the actual script file.

--- make_invoice.rb
+++ make_invoice.rb
@@ -4,20 +4,54 @@ require_relative "row"
 require_relative 'personal_info'
 
 class MakeInvoice
+  CYAN  = "\e[36m"
+  GREEN = "\e[92m"
+  RED   = "\e[31m"
 
-  def make
+  def run
+    rows = []
+    continue = true
+
+    printer(CYAN, "Welcome to Invoicer, enter as many line items as need.")
+    line_break(2)
+
+    while continue do
+      printer(GREEN, "Description [stuff I did]: ")
+      description = gets.chomp
+
+      printer(GREEN, "Date [01/01/2222]: ")
+      date = gets.chomp
+
+      printer(GREEN, "Hours [0-99]: ")
+      hours = gets.chomp.to_i
+
+      if [ description, date].all?{ |i| i != "" } && hours != 0
+        rows << Row.new(description, date, hours)
+      end
+
+      printer(RED, "Enter another item?  [y|n]: ")
+      continue = gets.chomp.downcase  == "y" ? true : false
+      line_break(2)
+    end
+
+    unless rows.empty?
+      make(rows)
+    end
+  end
+
+  def make(rows)
     itemizer = Itemizer.new(PersonalInfo::RATE)
-    itemizer.add_row(
-      Row.new("description", "01/02/2014", 4)
-    ) 
+    rows.each{ |row| itemizer.add_row(row) }
 
-    invoice_number = next_invoice_number 
+    invoice_number = next_invoice_number
 
     File.write(
       "unpaid/invoice_#{invoice_number}.txt",
       Invoicer.new(invoice_number, PersonalInfo::RATE, itemizer).invoice
     )
 
+    printer(CYAN, "Created invoice number #{invoice_number}")
+    line_break(1)
     increment_invoice_record(invoice_number)
   end
 
@@ -30,6 +64,14 @@ class MakeInvoice
   def increment_invoice_record(number)
     File.write("last_invoice.txt", number)
   end
+
+  def printer(color, text)
+    print "\t#{color}#{text}"
+  end
+
+  def line_break(count)
+    count.times{|t| puts }
+  end
 end
 
-MakeInvoice.new.make
+MakeInvoice.new.run
