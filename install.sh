d3518236d91537ba2c4df8d09f8f28b6c2b33a04
Greg McGuirk <bert.mcguirk@gmail.com>
2015-10-31 17:40:24 -0700

    add terminal table

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 install_file_if_missing() {
-  if [ ! -f $1 ]; then  
+  if [ ! -f $1 ]; then
     echo "making file $1"
-    cp $2 $1 
-  fi 
-} 
+    cp $2 $1
+  fi
+}
 
 install_directory_if_missing() {
-  if [ ! -d $1 ]; then 
-    echo "making directory $1" 
+  if [ ! -d $1 ]; then
+    echo "making directory $1"
     mkdir $1
   fi
 }
@@ -18,3 +18,4 @@ install_file_if_missing "personal_info.rb" "personal_info_template.rb"
 install_file_if_missing "last_invoice.txt" "last_invoice_template.txt"
 install_directory_if_missing "paid"
 install_directory_if_missing "unpaid"
+bundle install
